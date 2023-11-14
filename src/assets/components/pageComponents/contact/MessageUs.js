5499db8b5618a1b346ef765d543dddc72816b1b2
Andreas Fredriksson <afredriksson89@gmail.com>
2023-11-14 15:13:24 +0100

    form validation and form handling
    form validation and form handling

--- src/assets/components/pageComponents/contact/MessageUs.js
+++ src/assets/components/pageComponents/contact/MessageUs.js
@@ -1,7 +1,55 @@
-import React from 'react'
-import Button from '../../baseComponents/Button'
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const MessageUs = () => {
+    const [nameErrorMessage, setNameErrorMessage] = useState('')
+    const [emailErrorMessage, setEmailErrorMessage] = useState('')
+    const [messageErrorMessage, setMessageErrorMessage] = useState('')
+
+    const form = useFormik ({
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        },
+
+        validationSchema: Yup.object ({ 
+            name: Yup.string()
+                .required('Please fill in a name with atleast 2 characters')
+                .min(2, 'Name must be at least 2 characters long'),
+            email: Yup.string()
+                .required('Please fill in a valid email example: abc@domain.com')
+                .email('Please fill in a valid email')
+                .matches(/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/,),
+            message: Yup.string()
+                .required('Please fill in a message with atleast 10 characters')
+                .min(10, 'Message must be at least 10 characters long')
+        }),
+
+        onSubmit: async (values) => {
+            const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
+        
+            switch(result.status) {
+                case 200:
+                    alert('Your message has been sent!')
+                    break;
+                case 400:
+                    setNameErrorMessage('Please fill in a name with atleast 2 characters');
+                    setEmailErrorMessage('Please fill in a valid email');
+                    setMessageErrorMessage('Please fill in a message with atleast 10 characters');
+                    break;
+            }        
+        }
+    })
+                
+    
     return (
         <section className="message-us">
             <div className="container">
@@ -11,12 +59,15 @@ const MessageUs = () => {
                     <h2>for any information.</h2>
                 </div>
                 <div className="user-information">
-                    <form action="#">
-                        <input method="post" type="text" name="name" placeholder="Name*"></input>
-                        <input method="post" type="email" name="email" placeholder="Email*"></input>
-                        <textarea method="post" name="message" placeholder="Your message*"></textarea>
+                    <form onSubmit={form.handleSubmit} noValidate>
+                        <span className="error">{nameErrorMessage}</span>
+                        <input className={form.errors.name && form.touched.name ? 'error' : ''} type="text" name="name" placeholder={form.errors.name ? form.errors.name : 'Name*'} value={form.values.name} onChange={form.handleChange} onBlur={form.handleBlur}></input>
+                        <span className="error">{emailErrorMessage}</span>
+                        <input className={form.errors.email && form.touched.email ? 'error' : ''} type="email" name="email" placeholder={form.errors.email ? form.errors.email : 'Email*'} value={form.values.email} onChange={form.handleChange} onBlur={form.handleBlur}></input>
+                        <span className="error">{messageErrorMessage}</span>
+                        <textarea className={form.errors.message && form.touched.message ? 'error' : ''} type="text" name="message" placeholder={form.errors.message ? form.errors.message : 'Your message*'} value={form.values.message} onChange={form.handleChange} onBlur={form.handleBlur}></textarea>
+                        <button type="Submit">Send Message <i className="fa-regular fa-arrow-up-right"></i></button>
                     </form>
-                    <Button type="yellow" title="Send Message" url="/sendmessage" />
                 </div>
             </div>
         </section>
@@ -24,6 +75,10 @@ const MessageUs = () => {
 }
 
 export default MessageUs
+                    
+                        
+                            
+                       
 
                         
 
